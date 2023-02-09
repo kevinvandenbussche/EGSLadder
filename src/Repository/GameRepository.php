0abc63c1397215d4cc4229081fa09f64d36015a1
kevinvandenbussche <kevin.vandenbussche.pro@gmail.com>
2023-02-09 13:53:21 +0100

    ajout des requetes  pour la recuperation des jeux

--- src/Repository/GameRepository.php
+++ src/Repository/GameRepository.php
@@ -42,17 +42,15 @@ class GameRepository extends ServiceEntityRepository
 //    /**
 //     * @return Game[] Returns an array of Game objects
 //     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('g')
-//            ->andWhere('g.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('g.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findGame(): array
+    {
+        return $this->createQueryBuilder('g')
+            ->select('g.name', 'g.id')
+            ->orderBy('g.name', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 
 //    public function findOneBySomeField($value): ?Game
 //    {
