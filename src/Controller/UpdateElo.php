2d3a608ca5347ee39ca419a504bc15ab58730cb2
kevinvandenbussche <kevin.vandenbussche.pro@gmail.com>
2023-02-12 10:09:30 +0100

    ajout de la route update elo et mise a jour de la class

--- src/Controller/UpdateElo.php
+++ src/Controller/UpdateElo.php
@@ -4,42 +4,61 @@ namespace App\Controller;
 
 
 use App\Entity\ToPlay;
-use App\Repository\CalculationEloRepository;
-use App\Repository\GameRepository;
 use App\Repository\ToPlayRepository;
 use App\Repository\UserRepository;
 use App\Service\ApiLeagueOfLegends;
 use App\Service\ConvertEloLeagueOfLegends;
 use Doctrine\ORM\EntityManagerInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
 
-
+#[ApiResource(
+    collectionOperations: [
+        'get' => [
+            'path' => 'api/update-elo/{id}',
+            'status' => 201,
+        ],
+    ],
+)]
+#[AsController]
 class UpdateElo extends AbstractController
 {
-    #[Route('api/update-elo/{id}', name: 'update_elo', methods: "POST")]
+    private UserRepository $userRepository;
+    private ToPlayRepository $toPlayRepository;
+    private EntityManagerInterface $entityManager;
+    private ApiLeagueOfLegends $apiLeagueOfLegends;
+    private ConvertEloLeagueOfLegends $convertEloLeagueOfLegends;
 
-    public function updateElo(ToPlayRepository $toPlayRepository,
-                              $id,
-                              ApiLeagueOfLegends $apiLeagueOfLegends,
-                              EntityManagerInterface $entityManager,
-                              ConvertEloLeagueOfLegends $convertEloLeagueOfLegends,
-                              UserRepository $userRepository
-    ): JsonResponse
+    public function __construct(UserRepository $userRepository,
+                                ToPlayRepository $toPlayRepository,
+                                EntityManagerInterface $entityManager,
+                                ApiLeagueOfLegends $apiLeagueOfLegends,
+                                ConvertEloLeagueOfLegends $convertEloLeagueOfLegends)
+    {
+        $this->userRepository = $userRepository;
+        $this->toPlayRepository = $toPlayRepository;
+        $this->entityManager = $entityManager;
+        $this->apiLeagueOfLegends = $apiLeagueOfLegends;
+        $this->convertEloLeagueOfLegends = $convertEloLeagueOfLegends;
+    }
+
+
+    public function __invoke($id): Response
     {
         //je recupere les données utilisateurs de ma table Toplay
-        $toPlayOld = $toPlayRepository->findOneBy(['user'=>$id]);
+        $toPlayOld = $this->toPlayRepository->findOneBy(['user'=>$id]);
         $accountId = $toPlayOld->getAccountId();
         $startdate = $toPlayOld->getDateStart();
         $startEnd = $toPlayOld->getDateEnd();
         $game = $toPlayOld->getGame();
-        $datas = $apiLeagueOfLegends->apiLeaguesOfLegendsElo($accountId);
-        $user = $userRepository->find($id);
+        $datas = $this->apiLeagueOfLegends->apiLeaguesOfLegendsElo($accountId);
+        $user = $this->userRepository->find($id);
         $internalElo = 0;
         if(count($datas) > 0){
-            $internalElo = $convertEloLeagueOfLegends->getInternalElo($datas[0]['tier'], $datas[0]['rank'], $datas[0]['leaguePoints']);
+            $internalElo = $this->convertEloLeagueOfLegends->getInternalElo($datas[0]['tier'], $datas[0]['rank'], $datas[0]['leaguePoints']);
         }
         //je créer une nouvelle entité avec les données de l'utilisateur et je mets les nouvelles données recuperé de l'api
         $newToplay = new Toplay();
@@ -53,8 +72,8 @@ class UpdateElo extends AbstractController
         }
         $newToplay->setAccountId($accountId);
         $newToplay->setGame($game);
-        $entityManager->persist($newToplay);
-        $entityManager->flush();
+        $this->entityManager->persist($newToplay);
+        $this->entityManager->flush();
         return new JsonResponse(
             'mise jour elo reussi'
         );
