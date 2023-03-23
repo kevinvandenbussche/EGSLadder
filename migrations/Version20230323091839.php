94c1db14b804fc829787888f33d3c6bab8c22fb6
kevinvandenbussche <kevin.vandenbussche.pro@gmail.com>
2023-03-23 11:44:08 +0100

    ajout du bundle mercure pour le web socket

---
+++ migrations/Version20230323091839.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230323091839 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307FA76ED395');
+        $this->addSql('DROP INDEX IDX_B6BD307FA76ED395 ON message');
+        $this->addSql('ALTER TABLE message ADD receive_id INT DEFAULT NULL, CHANGE user_id send_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307F13933E7B FOREIGN KEY (send_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307F4CB96DCC FOREIGN KEY (receive_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_B6BD307F13933E7B ON message (send_id)');
+        $this->addSql('CREATE INDEX IDX_B6BD307F4CB96DCC ON message (receive_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307F13933E7B');
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307F4CB96DCC');
+        $this->addSql('DROP INDEX IDX_B6BD307F13933E7B ON message');
+        $this->addSql('DROP INDEX IDX_B6BD307F4CB96DCC ON message');
+        $this->addSql('ALTER TABLE message ADD user_id INT DEFAULT NULL, DROP send_id, DROP receive_id');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307FA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('CREATE INDEX IDX_B6BD307FA76ED395 ON message (user_id)');
+    }
+}
