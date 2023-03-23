73364527294f4f9f63f87535e4ceb8dcf17dad56
kevinvandenbussche <kevin.vandenbussche.pro@gmail.com>
2023-03-23 11:44:42 +0100

    ajout de la route et du controller pour la recuperation des messages

--- src/Controller/FindMessageByMessenger.php
+++ src/Controller/FindMessageByMessenger.php
@@ -2,7 +2,25 @@
 
 namespace App\Controller;
 
-class FindMessageByMessenger
+use App\Repository\MessageRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+
+#[AsController]
+class FindMessageByMessenger extends AbstractController
 {
 
+    private MessageRepository $messageRepository;
+    public function __construct(MessageRepository $messageRepository)
+    {
+        $this->messageRepository = $messageRepository;
+    }
+
+    public function __invoke(int $idUserSend, int $idUserReceive): Response
+    {
+        $data = $this->messageRepository->findMessageByMessenger($idUserSend, $idUserReceive);
+
+        return new Response(json_encode($data));
+    }
 }
